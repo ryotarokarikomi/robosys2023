0d2cf81221fb5b05207ac41cb72c9798977639c7
Ryotaro Karikomi <ryotaro.karikomi@gmail.com>
2023-10-26 12:51:57 +0900

    Add license

--- test.bash
+++ test.bash
@@ -1,5 +1,17 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2023 Ryotaro Karikomi
+# SPDX-License-Identifier: BSD-3-Clause
+
+ng(){
+  echo NG at line ${1}
+  res=1
+}
+
+res=0
 
 out=$(seq 5 | ./plus)
+[ "${out}" = 14 ] || ng "${LINENO}"
 
-[ "${out}" = 15 ]
+[ "${res}" = 0 ] && echo OK
+
+exit ${res}
