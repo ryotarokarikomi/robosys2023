b5a4cdd805e5700481be2b2a3f7f6ecdcbe44fa9
Ryotaro Karikomi <ryotaro.karikomi@gmail.com>
2023-11-18 17:36:00 +0900

    出力を配列に

--- test.bash
+++ test.bash
@@ -9,14 +9,22 @@ ng(){
 
 res=0
 
-out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng "${LINENO}"
+out=($(seq 5 | ./plus))
+[ "${out[0]}" = 15 ] || ng "${LINENO}"
+[ "${out[1]}" = 120 ] || ng "${LINENO}"
+
+echo out[0] = ${out[0]}
+echo out[1] = ${out[1]}
 
 out=$(echo あ | ./plus)
 [ "$?" = 1 ]      || ng "${LINENO}"
 [ "${out}" = "" ] || ng "${LINENO}"
 
 out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng "${LINENO}"
+[ "${out}" = "" ] || ng "${LINENO}"
+
+out=$(echo a | ./plus)
 [ "$?" = 1 ]      || ng "${LINENO}"
 [ "${out}" = "" ] || ng "${LINENO}"
 
